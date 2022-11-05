f68956da2f49e60157da6a72708399b09a98dec4
danil <jardus@bk.ru>
2022-11-05 16:57:16 +0300

    Fours

--- Задание/Program.cs
+++ Задание/Program.cs
@@ -1,35 +1,59 @@
-﻿
+﻿string[] Array = new string[] { };
+Console.Write("Введите количество слов, которые будут проверены на соответствие условиям:");
+int X = int.Parse(Console.ReadLine());
+Array = new string[X];
+for (int i = 0; i < Array.Length; i++)
+{
+    Console.Write($" Введите {i + 1} слово/число: ");
+    Array[i] = Console.ReadLine();
+}
+Console.WriteLine();
+Console.WriteLine("Введенные слова:");
+Console.WriteLine($"{PrintArray(Array)}");
+Console.WriteLine();
 
-string[] array = AskArray();
-string[] result = FindLessThan(array, 3);
-WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+int length = 3;  
 
-string[] FindLessThan(string[] input, int n) {
-    string[] output = new string[CountLessThan(input, n)];
+int CheckArray(string[] array, int length)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= length) result++;
+    }
+    return result;
+}
 
-    for(int i = 0, j = 0; i < input.Length; i++) {
-        if(input[i].Length <= n) {
-            output[j] = input[i];
-            j++;
+int numbers = CheckArray(Array, length); 
+
+string[] newArrayStrings = new string[numbers]; 
+NewArray(Array, newArrayStrings, length);
+
+void NewArray(string[] oldArray, string[] newArray, int lengthLimit)
+{
+    int temp = 0;
+    for (int i = 0; i < oldArray.Length; i++)
+    {
+        if (oldArray[i].Length <= lengthLimit)
+        {
+            newArray[temp] = oldArray[i];
+            temp++;
         }
     }
-
-    return output;
 }
 
-int CountLessThan(string[] input, int n) {
-    int count = 0;
-
-    for(int i = 0; i < input.Length; i++) {
-        if(input[i].Length <= n) {
-            count++;
-        }
+string PrintArray(string[] array)
+{
+    string result = string.Empty;
+    result = "[ ";
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += $"{array[i],1}";
+        if (i < array.Length - 1) result += ", ";
     }
-
-    return count;
+    result += " ]";
+    return result;
 }
 
-string[] AskArray() {
-    Write("Введите значения через пробел: ");
-    return ReadLine().Split(" ");
-}
+Console.WriteLine("Введенные слова/числа которые меньше, либо равны 3 знакам: ");
+Console.WriteLine($"{PrintArray(newArrayStrings)}");
